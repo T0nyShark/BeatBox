f2098d95ab7f65e86534b0458501b6b03f73e39f
T0nyShark <bencpb@gmail.com>
2018-10-11 21:15:05 +0300

    Ver 1.0

---
+++ app/src/main/java/com/industries/shark/beatbox/BeatBoxActivity.java
@@ -0,0 +1,15 @@
+package com.industries.shark.beatbox;
+
+import android.support.v4.app.Fragment;
+
+
+public class BeatBoxActivity extends SingleFragmentActivity {
+
+
+    @Override
+    protected Fragment createFragment() {
+        return BeatBoxFragment.newInstance();
+    }
+
+
+}
